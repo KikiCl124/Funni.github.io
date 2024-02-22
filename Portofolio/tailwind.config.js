8c6ae6c7ed680627b0c06a7f40cbe39ad100c609
Nikofu <65442182+KikiCl124@users.noreply.github.com>
2024-02-22 10:00:23 +0700

    whatever
    touching up

---
+++ Portofolio/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#991b1b',
+        seccondary: '#ef4444',
+        thirdextras:'#818cf8'
+      }
+    },
+  },
+  plugins: [],
+}
+
